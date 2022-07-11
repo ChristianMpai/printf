10b34a9a896ccccab41412d9539567b325e5ab39
ChristianMpai <tebogotebzana@gmail.com>
2022-07-11 08:17:21 -0700

    buffer of 1024 chars

--- main.h
+++ main.h
@@ -27,5 +27,6 @@ int print_uns_int(va_list arguments, char *buf, unsigned int ibuf);
 int print_oct(va_list arguments, char *buf, unsigned int ibuf);
 int print_hex_low(va_list arguments, char *buf, unsigned int ibuf);
 int print_hex_up(va_list arguments, char *buf, unsigned int ibuf);
+int print_buffer(char *buf, unsigned int nbuf);
 
 #endif
