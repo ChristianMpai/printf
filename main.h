e21228a8f30a1ed981adb72e76c46673a24ba6b6
ChristianMpai <tebogotebzana@gmail.com>
2022-07-11 08:52:59 -0700

    print string

--- main.h
+++ main.h
@@ -28,5 +28,7 @@ int print_oct(va_list arguments, char *buf, unsigned int ibuf);
 int print_hex_low(va_list arguments, char *buf, unsigned int ibuf);
 int print_hex_up(va_list arguments, char *buf, unsigned int ibuf);
 int print_buffer(char *buf, unsigned int nbuf);
+int print_string(va_list arguments, char *buf, unsigned int ibuf);
+int print_s_null(va_list arguments, char *buf, unsigned int ibuf);
 
 #endif
