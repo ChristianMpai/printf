9b290ff67b644e43f484d010b333398536224a86
ChristianMpai <tebogotebzana@gmail.com>
2022-07-11 05:30:06 -0700

    prints Unsigned decimal integer

--- main.h
+++ main.h
@@ -23,5 +23,6 @@ int print_function(const char *s, int index);
 int (*(const char *s, int index))(va_list, char *, unsigned int);
 int print_dec_int(va_list arguments, char *buf, unsigned int ibuf);
 int print_binary(va_list arguments, char *buf, unsigned int ibuf);
+int print_uns_int(va_list arguments, char *buf, unsigned int ibuf);
 
 #endif
