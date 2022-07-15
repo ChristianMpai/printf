469e5973a42c67e637c2d72d754f93ef0eb9cf2a
ChristianMpai <tebogotebzana@gmail.com>
2022-07-15 05:08:05 -0700

    function that produces output according to a format.

---
+++ ev_print.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * ev_print - returns the amount of identifiers.
+ * @s: argument indentifier
+ * @index: index of argument identifier.
+ * Return: amount of identifiers.
+ */
+int ev_print(const char *s, int index)
+{
+	print_t pr[] = {
+		{NULL, NULL},
+	};
+	int i = 0, j = 0, first_index;
+
+	first_index = index;
+	while (pr[i].type_arg)
+	{
+		if (s[index] == pr[i].type_arg[j])
+		{
+			if (pr[i].type_arg[j + 1] != '\0')
+				index++, j++;
+			else
+				break;
+		}
+		else
+		{
+			j = 0;
+			i++;
+			index = first_index;
+		}
+	}
+	return (j);
+}
